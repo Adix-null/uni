25639207758ab19c4729452cf8c6c86413a82552
Adomas Bieliūnas <adomas@stalas.lt>
2025-02-25 09:18:23 +0200

    Fixed and completed 2nd java exercise

--- semester_2/java_projects/Eismas/src/Main.java
+++ semester_2/java_projects/Eismas/src/Main.java
@@ -3,10 +3,17 @@ public class Main {
         TrafficUser t1 = new TrafficUser();
         t1.move(-10, 5);
         t1.println();
+        t1.move(Math.TAU / 8, 10.0f);
+        t1.println();
 
         TrafficUser t2 = new TrafficUser(7, 3, true);
         t2.println();
         t2.move(14, -2);
+        t2.setActive(false);
         t2.println();
+
+        TrafficUser t3 = new TrafficUser(2, 5);
+        t3.println();
+        System.out.println(TrafficUser.getInstanceCount() + " " + t3.getActive());
     }
 }
