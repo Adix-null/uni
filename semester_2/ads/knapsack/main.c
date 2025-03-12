ee6a2a5e080dc71e71966d3ece7586c2bfdf1991
Adomas Bieliūnas <adomas@stalas.lt>
2025-03-12 11:55:20 +0200

    init knapsack

---
+++ semester_2/ads/knapsack/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("test1\n");
+
+    int n[5] = {1, 4, 5, 2, 7};
+    int k[5] = {11, 4, 2, 9, 6};
+
+    int c = 17;
+
+    return 0;
+}
