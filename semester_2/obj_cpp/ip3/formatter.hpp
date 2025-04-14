2ebe0d8b9525f8c192e923b8fc34dee02d2a187d
adbi1341 <adomas@stalas.lt>
2025-04-14 15:17:58 +0300

    init ip3

---
+++ semester_2/obj_cpp/ip3/formatter.hpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include <cassert>
+#include <memory>
+
+#define CHAR_MAX_COUNT 10000
+
+static int dequeue_count = 0;
+static int error = 0;
+
+namespace ip3
+{
+    class formatter
+    {
+    private:
+        /* data */
+    public:
+        formatter(/* args */);
+        ~formatter();
+    };
+
+    formatter::formatter(/* args */)
+    {
+    }
+
+    formatter::~formatter()
+    {
+    }
+
+}
