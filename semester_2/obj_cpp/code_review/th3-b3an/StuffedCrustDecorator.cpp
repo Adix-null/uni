a9bcd28b5eedeb2a0449229a62ea5d124694a9c4
adbi1341 <adomas@stalas.lt>
2025-06-19 12:41:50 +0300

    reviewed 1st coleague's code

---
+++ semester_2/obj_cpp/code_review/th3-b3an/StuffedCrustDecorator.cpp
@@ -0,0 +1,62 @@
+#include "headers.h"
+
+StuffedCrustDecorator::StuffedCrustDecorator(
+    Pizza *p,
+    std::string type,
+    int level,
+    bool sauce) : PizzaDecorator(p), stuffingType(type), stuffingAmount(level), isExtraCrispy(sauce)
+{
+    validateStuffingType();
+    validateStuffingAmount();
+}
+
+std::string StuffedCrustDecorator::getDescription() const
+{
+    std::string desc = wrappedPizza->getDescription();
+    desc += "\n+ " + stuffingType + " (Stuffing Amount " + std::to_string(stuffingAmount) + ")";
+    if (isExtraCrispy)
+    {
+        desc += " Extra Crispy";
+    }
+    return desc;
+}
+
+double StuffedCrustDecorator::getCost() const
+{
+    double cost = wrappedPizza->getCost();
+    cost += stuffingAmount * 0.03;
+    return cost;
+}
+
+void StuffedCrustDecorator::bake() const
+{
+    wrappedPizza->bake();
+    logStep("Stuffing " + stuffingType + "...");
+    if (isExtraCrispy)
+    {
+        logStep("Drizzling garlic butter over pizza...");
+    }
+    logStep("Stuffing infusion complete.");
+}
+
+void StuffedCrustDecorator::validateStuffingType() const
+{
+    static const std::vector<std::string> allowed = {"Cheese", "Garlic Butter", "Cream Cheese"};
+    if (find(allowed.begin(), allowed.end(), stuffingType) == allowed.end())
+    {
+        throw StuffedDecoratorException("Unsupported stuffing type: " + stuffingType);
+    }
+}
+
+void StuffedCrustDecorator::validateStuffingAmount() const
+{
+    if (stuffingAmount < 30 || stuffingAmount > 150)
+    {
+        throw StuffedDecoratorException("Stuffing amount must be between 30 and 150.");
+    }
+}
+
+void StuffedCrustDecorator::logStep(const std::string &msg) const
+{
+    std::cout << "[StuffedDecorator] " << msg << std::endl;
+}
