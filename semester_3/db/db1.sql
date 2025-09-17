834c4bea9081baa36d9cffc46bd3bd60d917b650
Adomas B <74368343+Adix-null@users.noreply.github.com>
2025-09-17 15:09:48 +0300

    completed db1 exercise

--- semester_3/db/db1.sql
+++ semester_3/db/db1.sql
@@ -1,1 +1,7 @@
-SELECT ak, vardas, pavarde, Gimimo_data FROM stud.skaitytojas WHERE Gimimo_data < NOW() - INTERVAL '20 years';
+SELECT ak, 
+vardas || ' ' || pavarde AS vardas_ir_pavarde, 
+EXTRACT(YEAR FROM AGE(CURRENT_DATE, Gimimo_data)) AS Metai
+        FROM stud.skaitytojas 
+        WHERE Gimimo_data < CURRENT_DATE - INTERVAL '20 years'
+        AND LOWER(vardas) LIKE '%ta%' 
+        ORDER BY pavarde DESC ; 
