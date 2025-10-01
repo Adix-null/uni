bdca61ddc8fef93b068c376370d3ef92a415f3c6
Adomas Bieliūnas <adomas@stalas.lt>
2025-10-01 15:33:05 +0300

    continued 3rd exec

--- semester_3/db/db3.sql
+++ semester_3/db/db3.sql
@@ -1,8 +1,15 @@
-SELECT vardas, pavarde, leidykla, COUNT(*) AS egzemplioriu_skaicius
+SELECT a.vardas || ' ' || a.pavarde AS a_vardas_pav, leidykla, COUNT(*) AS egzemplioriu_skaicius,
+sj.vardas || ' ' || sj.pavarde AS sj_vardas_pav
 	FROM stud.autorius a
 	LEFT JOIN stud.knyga k
 		ON a.isbn = k.isbn
 	LEFT JOIN stud.egzempliorius e
 		ON k.isbn = e.isbn
+	LEFT JOIN stud.skaitymas sm
+		ON sm.egzempliorius = e.nr
+	LEFT JOIN stud.skaitytojas sj
+		ON sm.skaitytojas = sj.nr
 	GROUP BY leidykla, vardas, pavarde
-	ORDER BY egzemplioriu_skaicius DESC;
+	HAVING COUNT(*) > 2
+	ORDER BY egzemplioriu_skaicius DESC
+;
--- semester_3/db/test.sql
+++ semester_3/db/test.sql
@@ -1,1 +1,13 @@
-SELECT * FROM stud.skaitymas;
+SELECT a.vardas || ' ' || a.pavarde AS a_vardas_pav, 
+leidykla, e.nr,
+sj.vardas || ' ' || sj.pavarde AS sj_vardas_pav
+	FROM stud.autorius a
+	LEFT JOIN stud.knyga k
+		ON a.isbn = k.isbn
+	LEFT JOIN stud.egzempliorius e
+		ON k.isbn = e.isbn
+	LEFT JOIN stud.skaitymas sm
+		ON sm.egzempliorius = e.nr
+	LEFT JOIN stud.skaitytojas sj
+		ON sm.skaitytojas = sj.nr
+;
