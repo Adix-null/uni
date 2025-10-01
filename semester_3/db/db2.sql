65865b11ebdddb240e69901ddadd470fa2d74bd9
Adomas Bieliūnas <adomas@stalas.lt>
2025-10-01 14:33:56 +0300

    defended 2nd exercise

--- semester_3/db/db2.sql
+++ semester_3/db/db2.sql
@@ -1,4 +1,4 @@
-SELECT DISTINCT a.vardas, a.pavarde, k.pavadinimas, e.nr AS egzemplioriaus_numeris
+SELECT DISTINCT a.vardas, a.pavarde, k.pavadinimas, k.verte, e.nr AS egzemplioriaus_numeris
 FROM stud.skaitytojas st, stud.skaitymas s
 LEFT JOIN stud.egzempliorius e
 	ON s.egzempliorius = e.nr
@@ -8,7 +8,7 @@ LEFT JOIN stud.autorius a
 	ON k.isbn = a.isbn
 WHERE
     EXTRACT(YEAR FROM CURRENT_DATE) = EXTRACT(YEAR FROM s.grazinti)
-    AND  EXTRACT(MONTH FROM s.grazinti) = 11
+    AND  EXTRACT(MONTH FROM s.grazinti) = 10
     AND s.grazinta IS NULL
     ORDER BY vardas ASC;
 
