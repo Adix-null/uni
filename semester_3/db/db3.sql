33bb4a4135118028f6da6bd8a2ae05869d23d981
Adomas <adomas@stalas.lt>
2025-10-06 14:41:13 +0300

    completed 3.2 (going into headscratching territory)

--- semester_3/db/db3.sql
+++ semester_3/db/db3.sql
@@ -1,8 +1,13 @@
-SELECT vardas, pavarde, leidykla, COUNT(*) AS egzemplioriu_skaicius
+SELECT 
+    a.vardas || ' ' || a.pavarde AS a_vardas_pav,
+    k.leidykla,
+    COUNT(DISTINCT e.nr) AS egzemplioriu_kiekis,
+    COUNT(DISTINCT CASE WHEN sm.grazinta IS NULL THEN sm.nr END) AS skaitytoju_kiekis
 	FROM stud.autorius a
-	LEFT JOIN stud.knyga k
-		ON a.isbn = k.isbn
-	LEFT JOIN stud.egzempliorius e
-		ON k.isbn = e.isbn
-	GROUP BY leidykla, vardas, pavarde
-	ORDER BY egzemplioriu_skaicius DESC;
+	LEFT JOIN stud.knyga k ON a.isbn = k.isbn
+	LEFT JOIN stud.egzempliorius e ON k.isbn = e.isbn
+	LEFT JOIN stud.skaitymas sm ON sm.egzempliorius = e.nr
+	GROUP BY a_vardas_pav, k.leidykla
+		HAVING COUNT(DISTINCT e.nr) > 2
+	ORDER BY a_vardas_pav ASC
+;
--- semester_3/db/test.sql
+++ semester_3/db/test.sql
@@ -1,1 +1,26 @@
-SELECT * FROM stud.skaitymas;
+--egzemplioriu kiekis
+SELECT a.vardas || ' ' || a.pavarde AS a_vardas_pav,
+leidykla, COUNT(a.vardas) AS egzemplioriu_skaicius
+	FROM stud.autorius a
+	LEFT JOIN stud.knyga k
+		ON a.isbn = k.isbn
+	LEFT JOIN stud.egzempliorius e
+		ON k.isbn = e.isbn
+	GROUP BY leidykla, a_vardas_pav
+	HAVING COUNT(a.vardas) > 2
+	ORDER BY egzemplioriu_skaicius DESC
+;
+--skaitytoju tiems egzemplioriams kiekis
+SELECT a.vardas || ' ' || a.pavarde AS a_vardas_pav,
+leidykla, COUNT(sm.nr) as skaitytoju_kiekis
+	FROM stud.autorius a
+	LEFT JOIN stud.knyga k
+		ON a.isbn = k.isbn
+	LEFT JOIN stud.egzempliorius e
+		ON k.isbn = e.isbn
+	INNER JOIN stud.skaitymas sm
+		ON sm.egzempliorius = e.nr		
+	WHERE sm.grazinta IS NULL
+	GROUP BY a_vardas_pav, leidykla	
+	ORDER BY a_vardas_pav ASC
+;
