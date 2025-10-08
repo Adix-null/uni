68188bf728aef54abb95ab063d21de41c4fa32d0
Adomas Bieliūnas <adomas@stalas.lt>
2025-10-08 15:07:50 +0300

    defended 3rd exec

--- semester_3/db/db3.sql
+++ semester_3/db/db3.sql
@@ -2,10 +2,11 @@ SELECT
     a.vardas || ' ' || a.pavarde AS a_vardas_pav,
     k.leidykla,
     COUNT(DISTINCT e.nr) AS egzemplioriu_kiekis,
-    COUNT(DISTINCT CASE WHEN sm.grazinta IS NULL THEN sm.nr END) AS skaitytoju_kiekis
+    COUNT(CASE WHEN sm.grazinta IS NULL THEN sm.nr END) AS skaitytoju_kiekis,
+    SUM(DISTINCT k.verte) * COUNT(DISTINCT e.nr) as verte_sum
 	FROM stud.autorius a
-	LEFT JOIN stud.knyga k ON a.isbn = k.isbn
-	LEFT JOIN stud.egzempliorius e ON k.isbn = e.isbn
+	JOIN stud.knyga k ON a.isbn = k.isbn
+	JOIN stud.egzempliorius e ON k.isbn = e.isbn
 	LEFT JOIN stud.skaitymas sm ON sm.egzempliorius = e.nr
 	GROUP BY a_vardas_pav, k.leidykla
 		HAVING COUNT(DISTINCT e.nr) > 2
