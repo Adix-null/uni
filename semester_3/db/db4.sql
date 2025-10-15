6bf8e7cafd14be861293181c39976ac11f57a754
Adomas <adomas@stalas.lt>
2025-10-15 12:48:35 +0300

    > to <

--- semester_3/db/db4.sql
+++ semester_3/db/db4.sql
@@ -1,12 +1,13 @@
-SELECT sm.grazinti, COUNT(*) AS ta_diena_grazinti
+SELECT sm.grazinti,
+    COUNT(*) AS ta_diena_grazinti
 FROM stud.skaitymas sm
 GROUP BY sm.grazinti
-HAVING COUNT(*) > (
-    SELECT AVG(sub.cnt)
-    FROM (
-        SELECT COUNT(*) AS cnt
-        FROM stud.skaitymas
-        GROUP BY grazinti
-    ) AS sub
-)
+HAVING COUNT(*) < (
+        SELECT AVG(sub.cnt)
+        FROM (
+                SELECT COUNT(*) AS cnt
+                FROM stud.skaitymas
+                GROUP BY grazinti
+            ) AS sub
+    )
 ORDER BY COUNT(*) DESC;
