0ca78b8064e72745c4b165c16697a854f678fa2e
Adomas <adomas@stalas.lt>
2026-03-07 22:58:36 +0200

    init 2nd lab client, will make it into ftp

---
+++ semester_4/networks/lab2/client.c
@@ -0,0 +1,77 @@
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+#define BUFFLEN 1024
+
+int main(int argc, char *argv[])
+{
+    WSADATA wsadata;
+    unsigned int port;
+    int s_socket;
+    struct sockaddr_in server_address;
+
+    char buffer[BUFFLEN];
+
+    if (argc != 3)
+    {
+        fprintf(stderr, "USAGE: %s <ip> <port>\n", argv[0]);
+        exit(1);
+    }
+
+    port = atoi(argv[2]);
+
+    if ((port < 1) || (port > 65535))
+    {
+        printf("ERROR #1: invalid port specified.\n");
+        exit(1);
+    }
+
+    WSAStartup(MAKEWORD(2, 2), &wsadata);
+
+    /* Creation of client socket */
+    if ((s_socket = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    {
+        fprintf(stderr, "ERROR #2: cannot create socket.\n");
+        exit(1);
+    }
+
+    /* Clean server adress memory */
+    memset(&server_address, 0, sizeof(server_address));
+    server_address.sin_family = AF_INET;   // protocol is IP
+    server_address.sin_port = htons(port); // port set
+
+    /* Parse and set IP */
+    server_address.sin_addr.s_addr = inet_addr(argv[1]);
+    printf("Connecting to %s:%d ...\n", inet_ntoa(server_address.sin_addr), port);
+
+    /* Connect to server */
+    if (connect(s_socket, (struct sockaddr *)&server_address, sizeof(server_address)) < 0)
+    {
+        fprintf(stderr, "ERROR #4: error in connect().\n");
+        exit(1);
+    }
+    printf("Connected to server %s:%d\n", inet_ntoa(server_address.sin_addr), port);
+
+    printf("This is a simple FTP client, select a file to upload\n");
+    while(true)
+    {
+        memset(&buffer, 0, BUFFLEN);
+        printf("Enter path: ");
+        fgets(buffer, BUFFLEN, stdin);
+    }
+
+    send(s_socket, buffer, strlen(buffer), 0);
+
+    memset(&buffer, 0, BUFFLEN);
+    recv(s_socket, buffer, BUFFLEN, 0);
+    printf("Message from server: %s\n", buffer);
+
+    closesocket(s_socket);
+
+    WSACleanup();
+    return 0;
+}
---
+++ semester_4/networks/lab2/docs.md
@@ -0,0 +1,5 @@
+### Client
+Quickly run with 
+```bash
+gcc client.c -o client.exe -lws2_32 && ./client.exe 127.0.0.1 1790
+``` 
