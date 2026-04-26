c4d43d2f6f0a41004177a456d95e49e6f6ff562e
Adomas <adomas@stalas.lt>
2026-04-27 00:54:47 +0300

    nope im using a console app

---
+++ semester_4/networks/lab3/ospf/Program.cs
@@ -0,0 +1,29 @@
+﻿class Router(string name)
+{
+    public string Name { get; set; } = name;
+    public List<(Router router, int cost)> Neighbors { get; set; } = [];
+    public Dictionary<string, (string NextHop, int totalCost)> Routes { get; set; } = [];
+
+    public void AddNeighbor(Router neighbor, int cost)
+    {
+        if (!Neighbors.Any(n => n.router == neighbor))
+        {
+            Neighbors.Add((neighbor, cost));
+            neighbor.AddNeighbor(this, cost);
+        }
+    }
+    public void RemoveNeighbour(Router neighbor)
+    {
+        Neighbors.RemoveAll(n => n.router == neighbor);
+        neighbor.RemoveNeighbour(this);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("test");
+
+    }
+}
